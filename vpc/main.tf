5a95be5143180fb85b2cdb8901c2407c63d2123e
Krishna Teja Palla <63608191+krishna0852@users.noreply.github.com>
2024-03-31 13:11:29 +0530

    oneline-commenting modules

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,35 +1,38 @@
-module "vpc" {
+# maintain your modules in below structure while you're using this project 
+# note: as per this project 2 pblc subnets and one private subnet is mandatory while you're using this
+
+# module "vpc" {
     
-    source="./modules/aws-infra-network"
+#     source="./modules/aws-infra-network"
 
-    vpc_cidr_range="10.9.0.0/16"
+#     vpc_cidr_range="10.9.0.0/16"
 
-nat-pblc-instance ={
-  nat-subnet={
-     availability_zone = "ap-southeast-2a"
-     cidr_block        = "10.9.0.0/24"
-     name              = "pblc-A"
+# nat-pblc-instance ={
+#   nat-subnet={
+#      availability_zone = "ap-southeast-2a"
+#      cidr_block        = "10.9.0.0/24"
+#      name              = "pblc-A"
 
-  }
-}
+#   }
+# }
 
-elastic-ip-name="elp-1"
+# elastic-ip-name="elp-1"
 
-pblcsbnt-config = {
-  pblc-B = {
-    availability_zone = "ap-southeast-2a"
-    cidr_block        = "10.9.16.0/24"
-    name             = "pblc-B"
-  }
+# pblcsbnt-config = {
+#   pblc-B = {
+#     availability_zone = "ap-southeast-2a"
+#     cidr_block        = "10.9.16.0/24"
+#     name             = "pblc-B"
+#   }
   
-}
+# }
 
-prvtsbnt-config = {
-  prvt-A = {
-    availability_zone = "ap-southeast-2b"
-    cidr_block        = "10.9.32.128/26"
-     name              = "prvt-A"
-  }
-}
-}
+# prvtsbnt-config = {
+#   prvt-A = {
+#     availability_zone = "ap-southeast-2b"
+#     cidr_block        = "10.9.32.128/26"
+#      name              = "prvt-A"
+#   }
+# }
+# }
 
